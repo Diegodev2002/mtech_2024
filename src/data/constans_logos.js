bfbda89ec8c9de8403588e3a247a861a0c30f07e
Diegodev2002 <chila_diego@outlook.com>
2024-05-06 13:42:53 -0600

    Agregé  aliado estrategico y se cambio texto de baner de inicio

--- src/data/constans_logos.js
+++ src/data/constans_logos.js
@@ -2,30 +2,37 @@ const aliados_estrategicos = [
 	{
 		link: '',
 		src: '/img/logos/ESCUDO_UPIIG3.webp',
-		alt: 'Instituto',
+		alt: 'Escudo UPIIG3',
 		width: 110,
 		height: 120,
 	},
 	{
 		link: '',
 		src: '/img/logos/utsoe_logo.webp',
-		alt: 'Instituto',
+		alt: 'Logotipo UTSOE',
 		width: 400,
 		height: 210,
 	},
 	{
 		link: '',
 		src: '/img/logos/intituto_tecnologico.webp',
-		alt: 'Instituto',
+		alt: 'Logotipo Instituto Tecnologico ',
 		width: 180,
 		height: 190,
 	},
 	{
 		link: '',
 		src: '/img/logos/STEAM_QKIDS.webp',
-		alt: 'Instituto',
+		alt: 'Logotipo STEAM QKIDS',
 		width: 160,
 		height: 190,
+	},
+	{
+		link: '',
+		src: '/img/logos/idea_gto.webp',
+		alt: 'Logotipo de Idea Guanajuato',
+		width: 130,
+		height: 130,
 	},
 ];
 export { aliados_estrategicos };
