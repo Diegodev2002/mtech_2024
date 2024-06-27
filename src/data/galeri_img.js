c065fd6aa351fb14a2b0391a3278c676254f1e55
Diegodev2002 <chila_diego@outlook.com>
2024-06-27 00:44:43 -0600

    Modificacion completa de la pagina

---
+++ src/data/galeri_img.js
@@ -0,0 +1,46 @@
+const imageArrayUPIIG = [
+{href: '/img/UPIIG-IPN/upiig1.webp', width: 1024, height: 576, alt: 'UPIIG Image 1'},
+{href: '/img/UPIIG-IPN/upiig2.webp', width: 1024, height: 576, alt: 'UPIIG Image 2'},
+{href: '/img/UPIIG-IPN/upiig3.webp', width: 1024, height: 576, alt: 'UPIIG Image 3'},
+{href: '/img/UPIIG-IPN/upiig4.webp', width: 1024, height: 576, alt: 'UPIIG Image 4'},
+{href: '/img/UPIIG-IPN/upiig5.webp', width: 1024, height: 576, alt: 'UPIIG Image 5'},
+{href: '/img/UPIIG-IPN/upiig6.webp', width: 1024, height: 576, alt: 'UPIIG Image 6'},
+{href: '/img/UPIIG-IPN/upiig7.webp', width: 1024, height: 576, alt: 'UPIIG Image 7'},
+{href: '/img/UPIIG-IPN/upiig8.webp', width: 1024, height: 576, alt: 'UPIIG Image 8'},
+{href: '/img/UPIIG-IPN/upiig9.webp', width: 1024, height: 576, alt: 'UPIIG Image 9'},
+];
+const imageArrayUTSOE = [
+  {href: '/img/UTSOE/utsoe1.webp', width: 1024, height: 576, alt: 'UTSOE Image 1'},
+  {href: '/img/UTSOE/utsoe2.webp', width: 1024, height: 576, alt: 'UTSOE Image 2'},
+  {href: '/img/UTSOE/utsoe3.webp', width: 1024, height: 576, alt: 'UTSOE Image 3'},
+  {href: '/img/UTSOE/utsoe4.webp', width: 1024, height: 576, alt: 'UTSOE Image 4'},
+  {href: '/img/UTSOE/utsoe5.webp', width: 1024, height: 576, alt: 'UTSOE Image 5'},
+  {href: '/img/UTSOE/utsoe6.webp', width: 1024, height: 576, alt: 'UTSOE Image 6'},
+  {href: '/img/UTSOE/utsoe7.webp', width: 1024, height: 576, alt: 'UTSOE Image 7'},
+  {href: '/img/UTSOE/utsoe8.webp', width: 1024, height: 576, alt: 'UTSOE Image 8'},
+  {href: '/img/UTSOE/utsoe9.webp', width: 1024, height: 576, alt: 'UTSOE Image 9'},
+];
+const imageArrayITSPR = [
+  {href: '../img/ITSPR/itspr1.webp', width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: '/img/ITSPR/itspr2.webp', width: 1024, height: 576, alt: 'ITSPR Image 2'},
+  {href: '/img/ITSPR/itspr3.webp', width: 1024, height: 576, alt: 'ITSPR Image 3'},
+  {href: '/img/ITSPR/itspr4.webp', width: 1024, height: 576, alt: 'ITSPR Image 4'},
+  {href: '/img/ITSPR/itspr5.webp', width: 1024, height: 576, alt: 'ITSPR Image 5'},
+  {href: '/img/ITSPR/itspr6.webp', width: 1024, height: 576, alt: 'ITSPR Image 6'},
+  {href: '/img/ITSPR/itspr7.webp', width: 1024, height: 576, alt: 'ITSPR Image 7'},
+  {href: '/img/ITSPR/itspr8.webp', width: 1024, height: 576, alt: 'ITSPR Image 8'},
+  {href: '/img/ITSPR/itspr9.webp', width: 1024, height: 576, alt: 'ITSPR Image 9'},
+];
+const image2022 = [
+  {href: "/img/Mtech2023/final/11.webp", width: 1024, height: 576, alt: '2022'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+  {href: "/img/UPIIG-IPN/upiig8.webp", width: 1024, height: 576, alt: 'ITSPR Image 1'},
+];
+export {imageArrayUPIIG, imageArrayUTSOE, imageArrayITSPR, image2022};
