7cfa451bc3667c677b8a06be0142c5eb4baa5b1f
Jesus Zermeño <josezerrod@hotmail.com>
2024-07-25 11:03:34 -0600

    refactor: Update Finalists component layout and team information display

--- src/components/Finalists.jsx
+++ src/components/Finalists.jsx
@@ -23,9 +23,9 @@ export function Finalists() {
             </div>
             <div className="p-4 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-10">
                 {finalTeams[activeTab].teams.map((team, index) => (
-                    <div key={index} className=" flex flex-col items-center">
+                    <div key={index} className=" flex flex-col items-center justify-center">
                         <img src="/img/index/teamp.webp" alt={`${team.name}`} className='w-[220px] h-auto' />
-                        <div className="relative bottom-10 left-0 right-0 bg-black w-full h-[110px] text-white p-2 text-center">
+                        <div className="relative bottom-10 left-0 right-0 bg-black w-full h-[110px] text-white p-2 text-center flex flex-col justify-center items-center">
                             <p className="sm:text-lg text-xs font-[pointles]">{team.name}</p>
                             <p className="sm:text-sm text-xs ">{team.school}</p>
                         </div>
